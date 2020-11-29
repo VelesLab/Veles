00cb4e254896c8a954cb0d78cdbf24c0cf8d8143
Veles <balex_2000@mail.ru>
2020-11-29 23:27:05 +0700

    Реализова функцию draw_house
    Сочинил её реализацию.
    Еужно сделать еще три функции: для оснвания, стен, крыши.

---
+++ lec_5.py
@@ -0,0 +1,53 @@
+
+def main():
+    x, y = 300, 200
+    width, height = 200, 300
+
+    draw_house(x, y, width, height)
+
+
+def draw_house(x, y, width, height):
+    """
+    Функция рисует домик ролной ширины width и высоты height от опорной точки (x, y),
+    которая находится в нижней точки фундамента.
+    :param x: координата x середины домика
+    :param y: координата y низа фкндамента
+    :param width: полная ширина домика (фундамента или вылета крыши включены)
+    :param height: полеая высота от низа фундамента
+    :return: None
+    """
+    print('Типа рисую домик...', x, y, width, height)
+    foundation_height = 0.05 * height
+    walls_width = 0.9 * width
+    walls_height = 0.5 * height
+    roof_height = height - foundation_height - walls_height
+
+
+    draw_house_foundation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
+
+def draw_house_foundation(x, y, width, height):
+    """
+    Функция основание домика ролной ширины width и высоты height от опорной точки (x, y),
+    которая находится в нижней точки фундамента.
+    :param x: координата x середины фундамента
+    :param y: координата y низа фкндамента
+    :param width: полная ширина фундамента
+    :param height: полеая высота фундамента
+    :return: None
+    """
+    print('Типа рисую основание...', x, y, width, height)
+    pass
+
+
+def draw_house_walls(x, y, width, height):
+    print('Типа рисую стены...', x, y, width, height)
+    pass
+
+
+def draw_house_roof(x, y, width, height):
+    print('Типа рисую крышу...', x, y, width, height)
+    pass
+
+main()
